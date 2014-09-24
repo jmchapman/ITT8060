fdb6b04c59a2fe23ba3d75d6bf5f6cccf908f637
Juhan Ernits <juhan.ernits@ttu.ee>
2014-09-24 12:07:57 +0300

    Added coursework3

---
+++ coursework3.fsx
@@ -0,0 +1,38 @@
+(*
+
+  ITT8060 -- Advanced Programming 2013
+  Department of Computer Science
+  Tallinn University of Technology
+  ------------------------------------
+
+  Coursework 3: Using .Net libraries
+
+  ------------------------------------
+  Name:
+  Student ID:
+  ------------------------------------
+
+
+  Answer the questions below.  You answers to questions 1--4 should be
+  correct F# code written after the question. This file is an F#
+  script file, it should be possible to load the whole file at
+  once. If you can't then you have introduced a syntax error
+  somewhere.
+
+  This coursework will be graded. Please submit it as coursework3/coursework3.fsx
+  in the Git system. The deadline is Friday, October 3. Please submit only code (no png files)
+
+*)
+
+// 1. Write code to load data from the file coursework3.data.
+//    The file is comma separated and contains population data
+//    of towns (linn) and communes (vald) in different counties (maakond) of Estonia.
+
+// 2. Make functions that calculates for each county the number of people that live 
+//    in towns and the number of people that live in communes.
+
+// 3. Create a donut chart (a pie chart with a white area in the middle) that shows the
+//    percentage of people living in towns vs. counties in Estonia.
+
+// 4. Create a separate pie chart for each county where you show the percentages of total
+//    population living in towns vs counties.
