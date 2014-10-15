d076f975c2fc6f4f9c8295140a4c0118773380f1
Juhan Ernits <juhan.ernits@ttu.ee>
2014-10-15 12:12:40 +0300

    Added notes of lecture 7 and coursework 5.

---
+++ coursework5.fsx
@@ -0,0 +1,57 @@
+﻿(*
+
+  ITT8060 -- Advanced Programming 2014
+  Department of Computer Science
+  Tallinn University of Technology
+  ------------------------------------
+
+  Coursework 5: Data centric programs
+
+  ------------------------------------
+  Name:
+  Student ID:
+  ------------------------------------
+
+
+  Answer the questions below. You answers to the questions should be
+  correct F# code written after the question. This file is an F# script
+  file; it should be possible to load the whole file at once. If you
+  can't, then you have introduced a syntax error somewhere.
+
+  This coursework will be graded.
+
+  Commit and push your solution to the repository as file
+  coursework5.fsx in directory coursework5. The downloaded data should go in
+  atoms.xml in the coursework5 directory.
+
+  The deadline for completing the above procedure is Friday,
+  October 24, 2014.
+
+  We will consider the submission to be the latest version of the
+  appropriate files in the appropriate directory before the deadline
+  of a particular coursework.
+
+
+*)
+
+// 1. Download a snapshot of the Atom feed:
+//    http://www.theregister.co.uk/headlines.atom
+//
+//    Using the example from lecture7.fsx, write a data structure AtomPart in F#
+//    that you would use to represent such feeds. You should use discriminated a
+//    discriminated union and records in the data structure.
+
+// 2. Write a function that loads the downloaded XML file and parses it into
+//    the data structure designed in task 1. You need to use the constructs provided
+//    by System.Xml.Linq.
+
+// 3. Write an aggregate function that returns how many words each author 
+//    wrote on a particular day.
+
+// 4. Write a function that takes a list of key words and returns the authors of 
+//    stories containing at least one of the key words.
+
+// 5. Write a function that takes a list of key words and returns the authors of 
+//    stories containing all of the key words.
+
+
---
+++ lecture7.fsx
@@ -0,0 +1,154 @@
+(*
+
+   Lecture Oct 15, 2014: Data centric and behaviour centric
+   programs.
+
+   Based on Chapter 7 and Chapter 8 of RWFP.
+   
+*)
+
+type Rect =
+ { Left  : float32
+   Top   : float32
+   Width : float32
+   Height: float32
+ }
+
+// C# anonymous types
+// var smth = {Left = 1.0f, Top =  }
+
+let rc = {Left = 10.0f; Top = 10.0f ; Width = 100.0f ; Height=200.0f}
+
+rc.Left + rc.Width
+
+let rc2awk = {Left = rc.Left + 100.0f ; Top = rc.Top ;
+       Width = rc.Width; Height=rc.Height}
+
+let rc2 = {rc with Left = rc.Left + 100.0f}
+
+
+open System.Drawing
+
+type TextContent =
+  { Text : string
+    Font : Font }
+
+type Orientation = 
+  | Vertical
+  | Horizontal
+
+type DocumentPart =
+  | SplitPart of Orientation * list<DocumentPart>
+  | TitledPart of TextContent * DocumentPart
+  | TextPart of TextContent
+  | ImagePart of string
+
+let fntText = new Font("Verdana", 12.0f)
+let fntHead = new Font("Verdana", 15.0f)
+
+let doc = 
+  TitledPart({Text = "ITT8060 Class 2014"; 
+              Font = fntHead},
+              SplitPart(Vertical, 
+              [ImagePart("image.png");
+               TextPart({Text = "..." ; Font = fntText})]))
+
+#r "System.Xml.Linq.dll"
+open System.Xml.Linq
+
+let attr(node:XElement, name, defaultValue) =
+  let attr = node.Attribute(XName.Get(name))
+  if (attr <> null) then attr.Value else defaultValue
+
+let parseOrientation(node) = 
+  match attr(node,"orientation","") with
+  | "horizontal" -> Horizontal
+  | "vertical"   -> Vertical
+  | _            -> failwith "Unknown orientation!" 
+
+
+let parseFont(node) =
+  new Font("Verdana", 12.0f, FontStyle.Regular)
+
+let rec loadPart(node:XElement) =
+  match node.Name.LocalName with
+  | "titled" ->
+    let tx = {Text = attr(node,"title", "");
+              Font = parseFont(node) }
+    let body = loadPart (Seq.head(node.Elements()))
+    TitledPart(tx,body)
+  | "split"  -> 
+    let orient = parseOrientation node
+    let nodes = node.Elements() |> List.ofSeq |> List.map loadPart
+    SplitPart(orient,nodes)
+  | "text"   ->
+    TextPart({Text = node.Value; Font = parseFont node})
+  | "image"  -> 
+    ImagePart(attr(node,"filename",""))
+  | name -> failwith("Unknown node: "+name)
+
+let rec aggregateDocument f state docPart =
+  let state = f state docPart
+  match docPart with
+  | TitledPart(_,part) ->
+    aggregateDocument f state part
+  | SplitPart(_, parts) ->
+    List.fold (aggregateDocument f) state parts
+  | _ -> state
+
+let totalWords document = 
+  aggregateDocument (fun count part -> 
+    match part with
+    | TextPart(tx) | TitledPart(tx,_) ->
+      count + tx.Text.Split(' ').Length
+    | _ -> count)
+    0 document
+
+
+let doc2 = loadPart(XDocument.Load(@"c:\tmp\live7.xml").Root)
+
+totalWords doc2
+
+
+
+
+
+
+
+
+
+
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
